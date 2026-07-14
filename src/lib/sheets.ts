83df33d49e285317bc780876ce7a08fe68f51014
myoharin <myoharin@gmail.com>
2026-07-14 16:00:05 +0800

    Added export keyword

--- src/lib/sheets.ts
+++ src/lib/sheets.ts
@@ -7,55 +7,55 @@ const db_api_url = process.env.DB_API_URL;
 const test_db_api_url = process.env.TEST_DB_API_URL ? process.env.TEST_DB_API_URL : "";
 
 // CRUD functions straight to the end point
-async function createMembers(members: SheetMember[]): Promise<boolean> {
+export async function createMembers(members: SheetMember[]): Promise<boolean> {
   return false;
 }
-async function createAvailabilities(availabilities: SheetAvailability[]): Promise<boolean> {
+export async function createAvailabilities(availabilities: SheetAvailability[]): Promise<boolean> {
   return false;
 }
-async function createIdeas(ideas: SheetIdea[]): Promise<boolean> {
+export async function createIdeas(ideas: SheetIdea[]): Promise<boolean> {
   return false;
 }
-async function createEvents(events: SheetEvent[]): Promise<boolean> {
-  return false;
-}
-
-async function readMembers(uuids: string[] = []): Promise<SheetMember[]> {
-  return [];
-}
-async function readAvailabilities(uuids: string[] = []): Promise<SheetAvailability[]> {
-  return [];
-}
-async function readIdeas(uuids: string[] = []): Promise<SheetIdea[]> {
-  return [];
-}
-async function readEvents(uuids: string[] = []): Promise<SheetEvent[]> {
-  return [];
-}
-
-async function updateMembers(members: SheetMember[]): Promise<boolean> {
-  return false;
-}
-async function updateAvailabilities(availabilities: SheetAvailability[]): Promise<boolean> {
-  return false;
-}
-async function updateIdeas(ideas: SheetIdea[]): Promise<boolean> {
-  return false;
-}
-async function updateEvents(events: SheetEvent[]): Promise<boolean> {
+export async function createEvents(events: SheetEvent[]): Promise<boolean> {
   return false;
 }
 
-async function deleteMembers(uuids: string[]): Promise<boolean> {
+export async function readMembers(uuids: string[] = []): Promise<SheetMember[]> {
+  return [];
+}
+export async function readAvailabilities(uuids: string[] = []): Promise<SheetAvailability[]> {
+  return [];
+}
+export async function readIdeas(uuids: string[] = []): Promise<SheetIdea[]> {
+  return [];
+}
+export async function readEvents(uuids: string[] = []): Promise<SheetEvent[]> {
+  return [];
+}
+
+export async function updateMembers(members: SheetMember[]): Promise<boolean> {
   return false;
 }
-async function deleteAvailabilities(uuids: string[]): Promise<boolean> {
+export async function updateAvailabilities(availabilities: SheetAvailability[]): Promise<boolean> {
   return false;
 }
-async function deleteIdeas(uuids: string[]): Promise<boolean> {
+export async function updateIdeas(ideas: SheetIdea[]): Promise<boolean> {
   return false;
 }
-async function deleteEvents(uuids: string[]): Promise<boolean> {
+export async function updateEvents(events: SheetEvent[]): Promise<boolean> {
+  return false;
+}
+
+export async function deleteMembers(uuids: string[]): Promise<boolean> {
+  return false;
+}
+export async function deleteAvailabilities(uuids: string[]): Promise<boolean> {
+  return false;
+}
+export async function deleteIdeas(uuids: string[]): Promise<boolean> {
+  return false;
+}
+export async function deleteEvents(uuids: string[]): Promise<boolean> {
   return false;
 }
 
